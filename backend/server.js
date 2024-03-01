d4cd2425cea155812869fdbf30904357bec2bb08
Md Mehedi Hasan <116740305+DevMehedi-CE@users.noreply.github.com>
2024-03-01 18:58:35 +0600

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // all packages import 
+import  path  from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -15,6 +16,8 @@ import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -23,6 +26,12 @@ app.use(cookieParser());
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',usersRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 
 // app.get('/',(req,res)=>{
 //     res.send("Hello Backend World!")
